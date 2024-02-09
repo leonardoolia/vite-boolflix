23de46fa7d66b6bb091a6e985ca4ef58d81d1006
leonardoolia <leonardooliaa@gmail.com>
2024-02-09 16:40:51 +0100

    created store and data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script>
 import AppHeader from './AppHeader.vue';
 import AppMain from './AppMain.vue';
+import { store } from './assets/data/store';
+import { api } from './assets/data/index';
 export default {
   name: 'Boolflix',
   components: { AppHeader, AppMain }
---
+++ src/assets/data/index.js
@@ -0,0 +1,6 @@
+const api = {
+    baseUri: 'https://api.themoviedb.org/3',
+    apiKey: '91b83f31acd0e9563ed57c2b2bbcc0f1',
+}
+
+export { api }
